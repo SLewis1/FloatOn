f6a46085a4320c720553fa03785c11e2d0bbb510
SLewis1 <stevendanlewis@gmail.com>
2017-10-08 11:00:06 -0700

    added font

--- OLED_clock.ino
+++ OLED_clock.ino
@@ -4,6 +4,8 @@
 #include "RTClib.h"
 #include <Wire.h>
 RTC_PCF8523 rtc;
+#include <Fonts/FreeSerif24pt7b.h> 
+#define font    FreeSerif24pt7b
 
 #define OLED_CS 6 //was 10
 #define OLED_RESET 9
@@ -11,21 +13,25 @@ RTC_PCF8523 rtc;
 
 Adafruit_SSD1325 display(OLED_DC, OLED_RESET, OLED_CS);
 
-int textSize = 4;
+int textSize = 1;
 int sec = 0;
 int across = 0;
-
+int vspacing;
+int myHour;
 //#define seconds  //show seconds
+//#define seriald
 
 void setup() {
-//  Serial.begin(9600);
-//  delay(1000);
-//  Serial.println("OLED test");
+#ifdef seriald
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("OLED test");
+#endif
   
   display.setTextSize(textSize);
   display.setTextColor(1);
   display.setTextWrap(false);
-  
+  display.setFont(&font);
   display.begin(); 
   display.clearDisplay();
   display.display();
@@ -35,37 +41,44 @@ void setup() {
 void loop() {
   
   DateTime now = rtc.now();
-  Serial.print(now.hour(),DEC); Serial.print(":"); Serial.print(now.minute(),DEC); Serial.print(":"); Serial.println(now.second(),DEC);
-  display.setCursor(0,0);
+  #ifdef seriald
+    Serial.print(now.hour(),DEC); Serial.print(":"); Serial.print(now.minute(),DEC); Serial.print(":"); Serial.println(now.second(),DEC);
+  #endif
+  
 //hours
-  if (now.hour() > 12) {
-    display.print(now.hour()-12);
+ 
+  //convert to 12-hour time from 24-hour
+  if (now.hour() > 12)   
+  {
+    myHour = now.hour()-12;
   }
-  else {
-    display.print(now.hour());
+  else
+  {
+    myHour = now.hour();
   }
   
-  //display.setCursor(4*textSize,0);
+  //adjust horizontal spacing so display is centered
+  if (myHour < 10)
+  {
+    display.setCursor(22,36);
+  }
+  else
+  {
+    display.setCursor(10,36);
+  }
+  
+  display.print(myHour);
   display.print(":"); 
 
 //minutes
-  //display.setCursor(8*textSize,0);
+  //add leading 0 if needed
   if (now.minute() < 10) {
     display.print("0");
   }
+  
   display.print(now.minute()); 
- 
-/*//ampm 
-  if (now.hour() < 12) {
-    display.println("am");
-  }
-  else {
-    display.println("pm");
-  }
-*/
- //display.display();
 
-//seconds
+//seconds in bottom right corner
 #ifdef seconds
   display.setTextSize(1);
   display.setCursor(115,55);
@@ -77,28 +90,26 @@ void loop() {
 #endif //seconds
 
 //line going across
-//Serial.print("  ");Serial.println(2*now.second());
-//Serial.print("  ");Serial.println(across);
-
   across = floor(2.16*now.second());
+  vspacing = 50;
   if (now.minute() % 2 == 0)  //even minutes
   {
       //growing white bar
-      display.fillRect(0,36,across,2,1);
+      display.fillRect(0,vspacing,across,2,1);
       display.display();
       delay(500);
-      display.fillRect(across,36,1,2,1);
+      display.fillRect(across,vspacing,1,2,1);
       display.display();
       delay(500);
   }
   else // odd minutes
   {
       //growing black bar (white shrinking)
-      display.fillRect(0,36,128,2,1);
-      display.fillRect(0,36,across,2,0);
+      display.fillRect(0,vspacing,128,2,1);
+      display.fillRect(0,vspacing,across,2,0);
       display.display();
       delay(500);
-      display.fillRect(across,36,1,2,0);
+      display.fillRect(across,vspacing,1,2,0);
       display.display();
       delay(500);
   }
